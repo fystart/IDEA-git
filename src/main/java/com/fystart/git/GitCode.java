38470d536fdbe894db390d99cf318ac77bf03c81
fy <fystart@163.com>
2022-05-29 10:19:15 +0800

    hot-fix commit

--- src/main/java/com/fystart/git/GitCode.java
+++ src/main/java/com/fystart/git/GitCode.java
@@ -5,5 +5,6 @@ public class GitCode {
         System.out.println("hello,git");
         System.out.println("hello,git");
         System.out.println("hello,git");
+        System.out.println("hot-fix,git");
     }
 }
